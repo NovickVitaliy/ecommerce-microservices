2118fa19706b92d9054a82d6379d5a5705ceb42b
Vitalii Novik <novickvitaliy@gmail.com>
2024-05-13 18:00:51 +0300

    feat(global-exception-handler): implement

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,4 +1,7 @@
+using System.Net.Mime;
 using Catalog.API.Behaviours;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 var assembly = typeof(Program).Assembly;
@@ -15,12 +18,39 @@ builder.Services.AddCarter();
 
 builder.Services.AddMarten(options =>
 {
-    options.Connection(builder.Configuration.GetConnectionString("Database") 
-                   ?? throw new InvalidOperationException("Connection string was not provided"));
+    options.Connection(builder.Configuration.GetConnectionString("Database")
+                       ?? throw new InvalidOperationException("Connection string was not provided"));
 }).UseLightweightSessions();
 
 var app = builder.Build();
 
 app.MapCarter();
+
+app.UseExceptionHandler(errorHandlingApp =>
+{
+    errorHandlingApp.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if (exception == null)
+        {
+            return;
+        }
+
+        var problemDetails = new ProblemDetails()
+        {
+            Title = exception.Message,
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = exception.StackTrace
+        };
+
+        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+        logger.LogError(exception, exception.Message);
+
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/problem+json";
+
+        await context.Response.WriteAsJsonAsync(problemDetails);
+    });
+});
 
 app.Run();
